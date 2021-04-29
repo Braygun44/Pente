b5a09933a8a4ee647c505a2310eb193d82f3c05e
Brayden Meech <72529279+Braygun44@users.noreply.github.com>
2021-04-29 11:17:11 -0400

    variable renames and clicked square logic

--- pente.py
+++ pente.py
@@ -3,18 +3,18 @@ import tkinter as tk
 import squares as sq
 import gameSetup as setup
 
-BG_BLUE = (66, 218, 245)
-BOARD_AREA_COLOR = (242, 215, 167)
-BOARD_BORDER = (15, 15, 15)
-SCORE_B_COLOR = (255, 255, 255)
-SCORE_B_BORDER = (0, 0, 255)
+background_color = (66, 218, 245)
+game_board_background_color = (242, 215, 167)
+game_board_border_color = (15, 15, 15)
+scoreboard_background_color = (255, 255, 255)
+scoreboard_border_color = (0, 0, 255)
 
 root = tk.Tk()
-S_WIDTH = root.winfo_screenwidth()
-S_HEIGHT = root.winfo_screenheight() - 50
-print("the screen size is: ", S_WIDTH, S_HEIGHT)
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight() - 50
+print("the screen size is: ", screen_width, screen_height)
 
-setup = setup.penteSetup(S_WIDTH, S_HEIGHT)
+setup = setup.penteSetup(screen_width, screen_height)
 print(setup)
 
 pygame.init()
@@ -23,29 +23,35 @@ pygame.display.set_caption("Pynte")
 game = True
 
 def drawGameAreas():
-	pygame.draw.rect(screen, BOARD_AREA_COLOR, setup.getBoardParams())
+	pygame.draw.rect(screen, game_board_background_color, setup.getBoardParams())
 	drawGameSquares()
-	pygame.draw.rect(screen, BOARD_BORDER, setup.getBoardParams(), 2)
-	pygame.draw.rect(screen, SCORE_B_COLOR, setup.getScoreBParams())
-	pygame.draw.rect(screen, SCORE_B_BORDER, setup.getScoreBParams(), 2)
+	pygame.draw.rect(screen, game_board_border_color, setup.getBoardParams(), 2)
+	pygame.draw.rect(screen, scoreboard_background_color, setup.getScoreBParams())
+	pygame.draw.rect(screen, scoreboard_border_color, setup.getScoreBParams(), 2)
 
 #TODO add squares to array or something
 def drawGameSquares():
+	global squares_array
+	squares_array = []
 	for i in range(19):
 		for j in range(19):
 			temp_square = sq.Square(int(i * (setup.getBoardSizeSide() / 19) + setup.getSpacerValue()), int(j * (setup.getBoardSizeSide() / 19) + setup.getSpacerValue()), int(setup.getBoardSizeSide() / 19))
+			temp_square.setPos(i, j)
+			squares_array.append(temp_square)
 			drawSquare(temp_square, screen)
 
 def drawSquare(s, screen):
 	squareParams = s.getDrawParams()
-	squareX,squareY,squareW,squareH = squareParams
-	squareColor = s.getBGColor()
-	squareLC = s.getLineColor()
-	pygame.draw.rect(screen, squareColor, squareParams)
-	pygame.draw.line(screen, squareLC, (squareX + squareW / 2, squareY), (squareX + squareW / 2, squareY + squareH), 1)
-	pygame.draw.line(screen, squareLC, (squareX, squareY + squareH / 2), (squareX + squareW, squareY + squareH / 2), 1)
+	square_x,square_y,square_w,square_h = squareParams
+	square_color = s.getBGColor()
+	square_lc = s.getLineColor()
+	pygame.draw.rect(screen, square_color, squareParams)
+	pygame.draw.line(screen, square_lc, (square_x + square_w / 2, square_y), (square_x + square_w / 2, square_y + square_h), 1)
+	pygame.draw.line(screen, square_lc, (square_x, square_y + square_h / 2), (square_x + square_w, square_y + square_h / 2), 1)
 
 while game:
+	screen.fill(background_color)
+	drawGameAreas()
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			game = False
@@ -53,12 +59,14 @@ while game:
 		if event.type == pygame.MOUSEBUTTONUP:
 			pos = pygame.mouse.get_pos()
 			print(pos)
+			for squ in squares_array:
+				if squ.isInside(pos):
+					print(squ.getPos())
 		if event.type == pygame.VIDEORESIZE:
-			w, h = pygame.display.get_surface().get_size()
+			w,h = pygame.display.get_surface().get_size()
 			setup.updateSetup(w, h)
 			screen = pygame.display.set_mode((setup.getFinalW(), setup.getFinalH()), pygame.RESIZABLE)
-	screen.fill(BG_BLUE)
-	drawGameAreas()
+
 	pygame.display.flip()
 print("out!")
 pygame.quit()
--- squares.py
+++ squares.py
@@ -14,9 +14,23 @@ class Square:
 
 	def getDrawParams(self):
 		return(self.xLoc, self.yLoc, self.side, self.side)
+
 	def getState(self):
 		return self.myState
+
 	def getBGColor(self):
 		return self.bg_color
+
 	def getLineColor(self):
 		return self.lslategrey
+
+	def setPos(self, row, col):
+		self.row = row
+		self.col = col
+
+	def getPos(self):
+		return(self.row, self.col)
+
+	def isInside(self, coordinates):
+		if self.xLoc <= coordinates[0] < self.xLoc + self.side and self.yLoc <= coordinates[1] < self.yLoc + self.side:
+			return True
